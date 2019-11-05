e50fc79e8b64396205de9436c68ffd70a64c7722
Nobuhiro Funabiki <funabiki0819@gmail.com>
2019-11-05 14:41:09 -0800

    Add EstimationAnalysisVisualizer classes

---
+++ estimation_analysis_visualizer/EstimationAnalysisVisualizer2D.m
@@ -0,0 +1,26 @@
+classdef EstimationAnalysisVisualizer2D < EstimationAnalysisVisualizerBase
+    properties (SetAccess = protected)
+        
+    end
+    methods
+        function obj = EstimationAnalysisVisualizer2D(args)
+            obj.estimate_error_position_vector      = zeros(3,memory_size);
+            obj.estimate_error_velocity_vector      = zeros(3,memory_size);
+            obj.estimate_performance_index          = zeros(6,memory_size);
+        end
+
+        % Utility funcitons
+        function calculateEstimatePerformanceIndex(this, covariance_matrix, iMem)
+            performance_index = zeros(4,1);
+            est_err_position = this.estimate_error_position_vector(:,iMem);
+            est_err_velocity = this.estimate_error_velocity_vector(:,iMem);
+            for iVar = 1:2
+                performance_index(iVar, 1) = est_err_position(iVar) - sqrt(this.chi2*covariance_matrix(iVar, iVar));
+            end
+            for iVar = 3:4
+                performance_index(iVar, 1) = est_err_velocity(iVar-2) - sqrt(this.chi2*covariance_matrix(iVar, iVar));
+            end
+            this.estimate_performance_index(:,iMem) = performance_index;
+        end
+    end
+end
---
+++ estimation_analysis_visualizer/EstimationAnalysisVisualizerBase.m
@@ -0,0 +1,60 @@
+classdef EstimationAnalysisVisualizerBase < handle
+    properties (SetAccess = protected)
+        id
+        chi2
+        estimate_error_position_scalar
+        estimate_error_position_vector
+        estimate_error_velocity_scalar
+        estimate_error_velocity_vector
+        estimate_performance_index
+    end
+    methods
+        function obj = EstimationAnalysisVisualizerBase(args)
+            obj.id = args.id;
+            obj.chi2 = args.chi2;
+            memory_size = args.memory_size;
+            obj.estimate_error_position_scalar      = zeros(1,memory_size);
+            obj.estimate_error_velocity_scalar      = zeros(1,memory_size);
+        end
+
+        % Setters
+        function setEstimateErrorPositionScalar(this, position_true, position_est, iMem)
+            this.estimate_error_position_scalar(1,iMem) = norm(position_est - position_true);
+        end
+        function setEstimateErrorPositionVector(this, position_true, position_est, iMem)
+            this.estimate_error_position_vector(:,iMem) = position_est - position_true;
+        end
+        function setEstimateErrorVelocityScalar(this, velocity_true, velocity_est, iMem)
+            this.estimate_error_velocity_scalar(1,iMem) = norm(velocity_est - velocity_true);
+        end
+        function setEstimateErrorVelocityVector(this, velocity_true, velocity_est, iMem)
+            this.estimate_error_velocity_vector(:,iMem) = velocity_est - velocity_true;
+        end
+
+        % Getters
+        function output = getEstimateErrorPositionScalar(this, iMem)
+            output = this.estimate_error_position_scalar(1,iMem);
+        end
+        function output = getEstimateErrorVelocityScalar(this, iMem)
+            output = this.estimate_error_velocity_scalar(1,iMem);
+        end
+        function output = getPerformanceIndex(this, iMem)
+            output = this.estimate_performance_index(:,iMem);
+        end
+
+        % Visualization functions
+        function visualizeEstimateErrorPositionScalar(this, time_list, stamp)
+            plot(time_list, this.estimate_error_position_scalar, stamp)
+            hold on
+        end
+        function visualizeEstimateErrorVelocityScalar(this, time_list, stamp)
+            plot(time_list, this.estimate_error_velocity_scalar, stamp)
+            hold on
+        end
+        function visualizeEstimatePerformanceIndex(this, time_list, iVar, stamp)
+            plot(time_list, this.estimate_performance_index(iVar,:), stamp);
+            hold on
+        end
+        
+    end
+end
